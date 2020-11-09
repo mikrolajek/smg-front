795bb1be8161dca04536343fc29beb61a7832c6d
m.wajdzik@savoir.pl <m.wajdzik@savoir.pl>
2020-11-09 12:12:24 +0100

    Added favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,79 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <ApolloProvider client={apolloClient}>
       <Head>
         <title>Samsung QR/NFC</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Component {...pageProps} />
     </ApolloProvider>
@@ -41,6 +114,7 @@ MyApp.getInitialProps = async (appContext: AppContext) => {
     const cookieToken: string = cookies.get("token");
     console.log("Bearer ", cookieToken);
 
+    // console.log("COOKIES TOKEN", cookieToken);
     // try {
     //   jwt.verify(cookieToken, secret);
     // } catch (err) {
@@ -50,7 +124,6 @@ MyApp.getInitialProps = async (appContext: AppContext) => {
     //   res?.end();
     //   return { ...appProps };
     // }
-
     if (!(asPath == "/login")) {
       if (!cookieToken) {
         res?.writeHead(301, {
